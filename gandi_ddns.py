8449897e05b0c4f85cd647f71603d7be8cc9025e
Zhan Li <zhan.li@aol.com>
2022-09-19 22:33:20 -0500

    add email notification

--- gandi_ddns.py
+++ gandi_ddns.py
@@ -1,6 +1,6 @@
 import json
 import requests
-
+from messenger import Messenger
 
 def update_DNS(domain, api_key, record_name, record_type):
     external_ip = requests.get('https://api.ipify.org').content.decode('utf8')
@@ -12,11 +12,13 @@ def update_DNS(domain, api_key, record_name, record_type):
     for record in records.json():
         if (
                 record["rrset_type"] ==  record_type and
-                record["rrset_name"] ==  record_name and
-                record["rrset_values"][0] == external_ip
+                record["rrset_name"] ==  record_name
         ):
-            print("External ip stays the same. No further action")
-            return
+            exisiting_external_ip = record["rrset_values"][0]
+            break
+    if exisiting_external_ip == external_ip:
+        print("External ip stays the same. No further action")
+        return
     # update external ip if there is change
     data = {
         "rrset_name": record_name,
@@ -29,6 +31,17 @@ def update_DNS(domain, api_key, record_name, record_type):
         headers={"Authorization": f"Apikey {api_key}"},
         json=data
     )
+    Messenger(
+        "DNS update is successful.\n" +
+        f"\n External IP changed from {exisiting_external_ip} to {external_ip}"
+    ).sendEmail(
+        title="Grandi DNS Update report",
+        receiver_email=secrets["email"],
+        sender_email=secrets["email"],
+        sender_pwd=secrets["email_psd"],
+        sender_host=secrets["email_server"],
+    )
+
     print(response.json()["message"])
 
 
